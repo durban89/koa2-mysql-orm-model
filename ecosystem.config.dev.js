676d2eddd978bf68717c9210c19f21b680db13a3
durban <zhangdapeng89@126.com>
2017-02-15 16:15:00 +0800

    添加pm2 管理项目

---
+++ ecosystem.config.dev.js
@@ -0,0 +1,58 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name: "koa-mysql-orm-model-dev",
+      script: "app/app.babel.js",
+      error_file: "log/wap.out",
+      out_file: "log/wap.out",
+      watch: true,
+      ignore_watch: ["node_modules", "static"],
+      env: {
+        COMMON_VARIABLE: "true"
+      },
+      env_production: {
+        NODE_ENV: "development",
+        NODE_CONFIG_DIR: __dirname + "/app/config",
+        NODE_APP_INSTANCE: "",
+      }
+    },
+
+    // Second application
+    {
+      name      : "WEB",
+      script    : "web.js"
+    }
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/production",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
+    },
+    dev : {
+      user : "node",
+      host : "212.83.163.1",
+      ref  : "origin/master",
+      repo : "git@github.com:repo.git",
+      path : "/var/www/development",
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env dev",
+      env  : {
+        NODE_ENV: "dev"
+      }
+    }
+  }
+}
