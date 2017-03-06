6b371cbd34c80c754493904dfd88cfb35fcba428
durban <zhangdapeng89@126.com>
2017-03-06 17:59:57 +0800

    添加 Animal

---
+++ app/models/Animal.js
@@ -0,0 +1,42 @@
+import {
+  Model
+} from 'objection';
+
+export default class Animal extends Model {
+  static tableName = 'animal';
+
+  static jsonSchema = {
+    type: 'object',
+    required: ['name'],
+
+    properties: {
+      id: {
+        type: 'integer'
+      },
+      ownerId: {
+        type: ['integer', 'null']
+      },
+      name: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 255
+      },
+      species: {
+        type: 'string',
+        minLength: 1,
+        maxLength: 255
+      }
+    }
+  };
+
+  static relationMappings = {
+    owner: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: `${__dirname}/Person`,
+      join: {
+        from: 'Animal.ownerId',
+        to: 'Person.id'
+      }
+    }
+  }
+}
