f3cb6a614dca08e2c85199c96eeb557a8d3282c3
durban <zhangdapeng89@126.com>
2017-03-06 17:45:26 +0800

    创建 movie person_movie

--- app/migrations/20170302225637_create_movie.js
+++ app/migrations/20170302225637_create_movie.js
@@ -1,8 +1,16 @@
+'use strict';
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('movie', function(table){
+      table.increments('id').primary();
+      table.string('name');
+    })
+  ]);  
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTableIfExists('movie');
+  ]);
 };
